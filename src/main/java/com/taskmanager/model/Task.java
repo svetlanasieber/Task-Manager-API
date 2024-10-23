b701f519593b495b21a514b0b22b37997701b224
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-24 00:26:58 +0200

    Add files via upload

---
+++ src/main/java/com/taskmanager/model/Task.java
@@ -0,0 +1,30 @@
+package com.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    @ManyToMany
+    @JoinTable(
+        name = "task_dependencies",
+        joinColumns = @JoinColumn(name = "task_id"),
+        inverseJoinColumns = @JoinColumn(name = "dependency_id")
+    )
+    private Set<Task> dependencies = new HashSet<>();
+}
